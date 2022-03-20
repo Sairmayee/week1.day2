fd9910769c80442ca1e7d688631501b75daac29a
Sairmayee <64839434+Sairmayee@users.noreply.github.com>
2022-03-20 19:42:23 +0530

    Add files via upload
    Week1 Day2 assignment upload. Thank you.

---
+++ CalculatorAssignment.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class CalculatorAssignment {
+
+	public int add(int num1, int num2, int num3){
+		return num1+num2+num3;
+	}
+
+	public int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public double mult(double num1, double num2) {
+		return num1*num2;
+	}
+
+	public float div(int num1, int num2) {
+		return num1/num2;
+	}
+
+}
+
---
+++ MyCalculator.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+//import CalculatorAssignment;
+
+public class MyCalculator {
+	public static void main(String[] args) {
+		
+	
+	CalculatorAssignment calc = new CalculatorAssignment();
+	System.out.println(calc.add(12,11,10));
+	System.out.println(calc.sub(3456, 1));
+	System.out.println(calc.mult(25, 25));
+	System.out.println(calc.div(12345, 5));
+
+}
+}
